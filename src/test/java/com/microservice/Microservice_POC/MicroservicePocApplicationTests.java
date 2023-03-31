6d3acdb1665826d47e3feb2ffea1a029066522f4
TN456 <129092424+TN456@users.noreply.github.com>
2023-03-31 12:51:45 +0530

    Initial commit

---
+++ src/test/java/com/microservice/Microservice_POC/MicroservicePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.Microservice_POC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
