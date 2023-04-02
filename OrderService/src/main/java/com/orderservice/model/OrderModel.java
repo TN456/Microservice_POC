81924c236964c0bfa8cd8591e479547b970c24aa
TN456 <129092424+TN456@users.noreply.github.com>
2023-04-02 13:44:06 +0530

    Added dependency

--- OrderService/src/main/java/com/orderservice/model/OrderModel.java
+++ OrderService/src/main/java/com/orderservice/model/OrderModel.java
@@ -1,13 +1,11 @@
 package com.orderservice.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 
 import java.util.Date;
 import java.util.List;
