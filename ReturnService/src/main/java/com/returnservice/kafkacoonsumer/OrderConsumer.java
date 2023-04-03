d84e6237a6a7e45be0b0ceb1be592d9ff3b354ea
Adarsh8672 <adarshmaharana2000@gmail.com>
2023-04-03 15:47:06 +0530

    Added ReturnServiceApplication

--- ReturnService/src/main/java/com/returnservice/kafkacoonsumer/OrderConsumer.java
+++ ReturnService/src/main/java/com/returnservice/kafkacoonsumer/OrderConsumer.java
@@ -7,8 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import com.springboot.first.kafka.payload.User;
     @Service
     public class OrderConsumer {
 
