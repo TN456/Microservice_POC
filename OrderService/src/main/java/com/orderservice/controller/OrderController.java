b24fcf35da5849a04409444c2100b6ed177474a7
TN456 <129092424+TN456@users.noreply.github.com>
2023-04-03 11:50:56 +0530

    Modified Controller class

--- OrderService/src/main/java/com/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/orderservice/controller/OrderController.java
@@ -2,23 +2,33 @@ package com.orderservice.controller;
 
 import com.orderservice.model.OrderModel;
 import com.orderservice.repository.OrderRepository;
+import com.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class OrderController {
 
     @Autowired
-    private OrderRepository orderRepository;
+    private OrderService orderService;
 
-    @PostMapping("/create")
+    @PostMapping("/createOrder")
     public ResponseEntity<OrderModel> saveOrder(@RequestBody OrderModel orderModel){
-        return new ResponseEntity<OrderModel>(orderRepository.save(orderModel), HttpStatus.CREATED);
+        return new ResponseEntity<OrderModel>(orderService.saveOrder(orderModel), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAllOrders")
+    public List<OrderModel> getAllOrders(){
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/OrderById/{id}")
+    public ResponseEntity<OrderModel> getOrderById(@PathVariable("orderNumber") String orderNumber){
+        return new ResponseEntity<OrderModel>(orderService.getOrderById(orderNumber),HttpStatus.OK);
     }
 }
