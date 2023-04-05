2eb1814e43175553b5620947832da70b4032b70e
TN456 <129092424+TN456@users.noreply.github.com>
2023-04-05 17:43:36 +0530

    Added keycloak configuration and roles allowed

--- OrderService/src/main/java/com/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/orderservice/controller/OrderController.java
@@ -6,8 +6,8 @@ import com.orderservice.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 
@@ -21,19 +21,30 @@ public class OrderController {
     @Autowired
     private OrderProducer orderProducer;
 
-    @PostMapping("/createOrder")
-    public ResponseEntity<OrderModel> saveOrder(@Valid @RequestBody OrderModel orderModel) {
-        OrderModel savedOrder = orderService.saveOrder(orderModel);
+    @PostMapping("/createOrder/myntra")
+    @PreAuthorize("hasAuthority('myntra user')")
+    public ResponseEntity<OrderModel> saveOrderMyntra(@Valid @RequestBody OrderModel orderModel) {
+        OrderModel savedOrder = orderService.saveOrderMyntra(orderModel);
+        orderProducer.sendMessage(savedOrder);
+        return new ResponseEntity<OrderModel>(savedOrder, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/createOrder/flipkart")
+    @PreAuthorize("hasAuthority('flipkart user')")
+    public ResponseEntity<OrderModel> saveOrderFlipKart(@Valid @RequestBody OrderModel orderModel) {
+        OrderModel savedOrder = orderService.saveOrderFlipkart(orderModel);
         orderProducer.sendMessage(savedOrder);
         return new ResponseEntity<OrderModel>(savedOrder, HttpStatus.CREATED);
     }
 
     @GetMapping("/getAllOrders")
+    @PreAuthorize("hasAuthority('admin')")
     public List<OrderModel> getAllOrders() {
         return orderService.getAllOrders();
     }
 
     @GetMapping("/OrderById/{orderNumber}")
+    @PreAuthorize("hasAnyRole('myntra user', 'flipkart user')")
     public ResponseEntity<OrderModel> getOrderById(@PathVariable("orderNumber") String orderNumber) {
         return new ResponseEntity<OrderModel>(orderService.getOrderById(orderNumber), HttpStatus.OK);
     }
