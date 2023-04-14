8a45c58e7b49739964812782661aad839039c7e5
Adarsh8672 <adarshmaharana2000@gmail.com>
2023-04-14 10:50:03 +0530

    Added exception for access denied Added date field in the exception

--- ReturnService/src/main/java/com/returnservice/exception/GlobalExceptionHandler.java
+++ ReturnService/src/main/java/com/returnservice/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +30,8 @@ errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
 
 errorObject.setMessage(ex.getMessage());
 
+    errorObject.setTimestamp(new Date());
+
 return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.NOT_FOUND);
 }
 
@@ -40,6 +43,8 @@ ErrorObject errorObject = new ErrorObject();
 errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
 
 errorObject.setMessage(ex.getMessage());
+
+    errorObject.setTimestamp(new Date());
 
 return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.BAD_REQUEST);
 }
@@ -53,6 +58,8 @@ errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
 errorObject.setMessage(ex.getMessage());
 
+    errorObject.setTimestamp(new Date());
+
 return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
 }
 
@@ -62,6 +69,7 @@ Map<String, Object> body = new HashMap<String, Object>();
 body.put("Status Code", HttpStatus.BAD_REQUEST.value());
 List<String> errors = ex.getBindingResult().getFieldErrors().stream().map(x -> x.getDefaultMessage()).collect(Collectors.toList());
 body.put("message", errors);
+    body.put("timestamp", new Date());
 return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
 }
 }
