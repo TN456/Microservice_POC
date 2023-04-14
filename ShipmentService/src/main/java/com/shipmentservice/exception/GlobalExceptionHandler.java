993586328419e5589dcc1460de89cbc4348c3c97
TN456 <129092424+TN456@users.noreply.github.com>
2023-04-14 13:43:38 +0530

    Added time to exception

--- ShipmentService/src/main/java/com/shipmentservice/exception/GlobalExceptionHandler.java
+++ ShipmentService/src/main/java/com/shipmentservice/exception/GlobalExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +29,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errorObject.setStatusCode(HttpStatus.NOT_FOUND.value());
 
         errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(new Date());
 
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.NOT_FOUND);
     }
@@ -40,6 +42,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errorObject.setStatusCode(HttpStatus.BAD_REQUEST.value());
 
         errorObject.setMessage(ex.getMessage());
+        errorObject.setTimestamp(new Date());
 
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.BAD_REQUEST);
     }
@@ -52,7 +55,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         errorObject.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
         errorObject.setMessage(ex.getMessage());
-
+        errorObject.setTimestamp(new Date());
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
