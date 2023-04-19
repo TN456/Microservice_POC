b598d4ce1259da359d7c3e46ec6bbe51bd5d0c9b
TN456 <129092424+TN456@users.noreply.github.com>
2023-04-19 17:53:00 +0530

    Added Internal Api setting

--- OrderService/src/main/java/com/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/orderservice/controller/OrderController.java
@@ -1,9 +1,12 @@
 package com.orderservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.orderservice.exception.ResourceNotFoundException;
 import com.orderservice.kafka.OrderProducer;
 import com.orderservice.model.ErrorObject;
 import com.orderservice.model.OrderModel;
 import com.orderservice.service.OrderService;
+import com.reservationservice.model.ReservationModel;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,9 +17,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.WebRequest;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Date;
 import java.util.List;
 
@@ -34,7 +44,6 @@ public class OrderController {
     @Autowired
     private OrderProducer orderProducer;
 
-//    @PostMapping("/createOrder/myntra")
     @PreAuthorize("hasAuthority('Myntra User')")
     @Operation(
             summary = "Create Myntra Order REST API",
@@ -44,9 +53,19 @@ public class OrderController {
             responseCode = "201",
             description = "HTTP Status 201 CREATED"
     )
-    @PostMapping(value = "/createOrder/myntra", consumes = { MediaType.APPLICATION_XML_VALUE }, produces = {
-            MediaType.APPLICATION_JSON_VALUE })
-    public ResponseEntity<OrderModel> saveOrderMyntra(@Valid @RequestBody OrderModel orderModel) {
+    @PostMapping(value = "/createOrder/myntra")
+    public ResponseEntity<OrderModel> saveOrderMyntra(@Valid @RequestBody OrderModel orderModel) throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        String reservationApiUrl = "http://localhost:9096/api/reservations/" + orderModel.getOrderNumber();
+        HttpRequest reservationRequest = HttpRequest.newBuilder()
+                .uri(URI.create(reservationApiUrl))
+                .build();
+        HttpResponse<InputStream> reservationResponse = httpClient.send(reservationRequest, HttpResponse.BodyHandlers.ofInputStream());
+        if (reservationResponse.statusCode() != HttpStatus.OK.value()) {
+            throw new ResourceNotFoundException("Reservation not found with the order number: "+orderModel.getOrderNumber());
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        ReservationModel reservation = objectMapper.readValue(reservationResponse.body(), ReservationModel.class);
         OrderModel savedOrder = orderService.saveOrderMyntra(orderModel);
         orderProducer.sendMessage(savedOrder);
         return new ResponseEntity<OrderModel>(savedOrder, HttpStatus.CREATED);
@@ -61,11 +80,11 @@ public class OrderController {
 
         errorObject.setTimestamp(new Date());
 
-        return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.FORBIDDEN);
+        return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.FORBIDDEN);
     }
 
-    @PostMapping(value = "/createOrder/flipkart", consumes = { MediaType.APPLICATION_XML_VALUE }, produces = {
-            MediaType.APPLICATION_JSON_VALUE })
+    @PostMapping(value = "/createOrder/flipkart", consumes = {MediaType.APPLICATION_XML_VALUE}, produces = {
+            MediaType.APPLICATION_JSON_VALUE})
     @PreAuthorize("hasAuthority('Flipkart User')")
     @Operation(
             summary = "Create Flipkart Order REST API",
@@ -76,6 +95,12 @@ public class OrderController {
             description = "HTTP Status 201 CREATED"
     )
     public ResponseEntity<OrderModel> saveOrderFlipKart(@Valid @RequestBody OrderModel orderModel) {
+        RestTemplate restTemplate = new RestTemplate();
+        String reservationApiUrl = "http://localhost:9096/api/reservations/" + orderModel.getOrderNumber();
+        ResponseEntity<ReservationModel> response = restTemplate.getForEntity(reservationApiUrl, ReservationModel.class);
+        if (response.getStatusCode() != HttpStatus.OK) {
+            throw new ResourceNotFoundException("Reservation not found with the order number: " + orderModel.getOrderNumber());
+        }
         OrderModel savedOrder = orderService.saveOrderFlipkart(orderModel);
         orderProducer.sendMessage(savedOrder);
         return new ResponseEntity<OrderModel>(savedOrder, HttpStatus.CREATED);
@@ -120,8 +145,23 @@ public class OrderController {
             description = "HTTP Status 200 SUCCESS"
     )
     public ResponseEntity<OrderModel> updateOrder(@RequestBody OrderModel orderModel, @PathVariable("orderNumber") String orderNumber) {
-        OrderModel updateOrder = orderService.updateOrder(orderModel,orderNumber);
+        OrderModel updateOrder = orderService.updateOrder(orderModel, orderNumber);
         orderProducer.sendMessage(orderService.updateOrder(orderModel, orderNumber));
         return new ResponseEntity<OrderModel>(updateOrder, HttpStatus.OK);
+    }
+
+    @PutMapping("/deleteOrder/{orderNumber}")
+    @PreAuthorize("hasAuthority('Admin')")
+    @Operation(
+            summary = "Delete Order REST API",
+            description = "Delete Order REST API is used to delete a particular order in the database"
+    )
+    @ApiResponse(
+            responseCode = "204",
+            description = "HTTP Status 204 SUCCESS"
+    )
+    public ResponseEntity<?> deleteOrder(@PathVariable("orderNumber") String orderNumber){
+        orderService.deleteOrder(orderNumber);
+        return new ResponseEntity<>("Order deleted Successfully",HttpStatus.NO_CONTENT);
     }
 }
